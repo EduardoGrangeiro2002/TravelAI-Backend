26d1aecac51bf5cc91540a182df06affc0cb3b45
Eduardo <edu.grangeiro2002@gmail.com>
2024-05-01 08:18:10 -0300

    fix: Trade Portuguese prompt to english

--- lib/utils/prompt.rb
+++ lib/utils/prompt.rb
@@ -1,33 +1,33 @@
 module Utils
   class Prompt
     def self.itinerary(start_date, end_date)
-      @@prompt << "\nMe monte um roteiro de viagem para meu destino considerando que irei ficar das datas #{start_date} até #{end_date}"
+      @@prompt << "\nCreate a travel itinerary for my destination considering that I will stay from dates #{start_date} to #{end_date}"
       self
     end
 
     def self.best_way(origin)
-      @@prompt << "\nConsiderando que estou em #{origin} quais os melhores meios que tenho para chegar
-      até meu destino! Traga os prós e contra de cada meio. Se for por aeroporto me traga qual o melhor aeroporto perto do local que eu estou e o mais próximo até o destino"
+      @@prompt << "\nConsidering that I am in #{origin}, what are the best ways I can get there?
+      until my destination! Bring the pros and cons of each method. If it is by airport, tell me what is the best airport near where I am and the closest to the destination?"
       self
     end
 
     def self.locamotion_local()
-      @@prompt << "\nQuais é os meios de locamoção disponíveis na região"
+      @@prompt << "\nWhat means of transportation are available in the region?"
       self
     end
 
     def self.violence_info()
-      @@prompt << "\nQual é o índice de violência dessa região e quais cuidados eu devo ter."
+      @@prompt << "\nWhat is the violence rate in this region and what precautions should I take."
       self
     end
 
     def self.weather(start_date)
-      @@prompt << "\nQual é o clima da região para data #{start_date} do ano, dado o histórico do local."
+      @@prompt << "\nWhat is the region's climate for date #{start_date} of the year, given the location's history."
       self
     end
 
     def self.start_prompt(city, country)
-      @@prompt = "Estou viajando para #{city} no país #{country} e preciso que me traga as seguintes informações:"
+      @@prompt = "I am traveling to #{city} in country #{country} and I need you to bring me the following information:"
       self
     end
 
